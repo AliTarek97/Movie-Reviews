fc0875856ba91b5c1c67cdfb67440a9477903901
Ali <aly_helmy2015@hotmail.com>
2020-02-23 02:47:33 +0200

    Testing Signup and login

--- src/server.js
+++ src/server.js
@@ -5,6 +5,8 @@ const app = require('./app');
 require('./db/mongoose');
 const port = process.env.PORT || 3000;
 
-app.listen(port , () => {
+const server = app.listen(port , () => {
     console.log(`Listening on port ${port}`);
 })
+
+module.exports = server;
---
+++ tests/user.test.js
@@ -0,0 +1,56 @@
+const request = require('supertest');
+let server ;
+const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
+const User = require('../src/models/user');
+
+const userOne = {   
+    email:'ali@example.com',
+    password:'123456'
+}
+
+beforeEach(async () => { 
+    server = require('../src/server');
+})
+
+afterEach(async () => {
+    await User.deleteMany();
+    await server.close(); 
+});
+
+describe('Signup and login user' , () => {
+    it('should signup a new user' ,  async () => {
+        const response = await request(server)
+                        .post('/api/users/signup')
+                        .send({
+                            email: "ahmed@example.com",
+                            password: "123456"
+                        })
+                        .expect(201);
+        
+        const user =  await User.findById(response.body.user._id);
+        expect(user).not.toBeNull;
+    
+        expect(response.body).toMatchObject({
+            user: {
+                _id: user._id.toHexString(),
+                email: "ahmed@example.com",         
+            }
+        })
+    })
+    
+    it('should login a user' ,  async () => {
+        await new User(userOne).save();
+        const response = await request(server)
+                        .post('/api/users/login')
+                        .send({
+                            email: userOne.email,
+                            password: userOne.password
+                        })
+                        .expect(200);
+        const user =  await User.findById(response.body.user._id);
+        expect(user).not.toBeNull;
+    
+        expect(response.body.token).toBe(user.token)
+    })
+})
